0d1a59995877cb3cae1df92c82816eb3a3e86957
AMRESH-SHARMA <amreshaiio@gmail.com>
2023-09-04 22:00:58 +0530

    vca4 db integrated

--- src/utility/apiConfig.js
+++ src/utility/apiConfig.js
@@ -1,7 +1,17 @@
 
 
-export const BASE_URL = 'http://localhost:8080/api'
+// export const BASE_URL = 'http://localhost:8080/api'
+
+// export const CURRENT_BASE_URL = 'http://localhost:3000'
+
+// PRODUCTION
+
+export const BASE_URL = 'http://34.31.210.3:8084/api'
+
+export const CURRENT_BASE_URL = 'http://3.110.190.235:3000/'
 
 
-export const CURRENT_BASE_URL = 'http://localhost:3000'
+
+
+
 
