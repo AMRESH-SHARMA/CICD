7b522af841626ac6b0d133f64338215dbb7da54f
AMRESH-SHARMA <amreshaiio@gmail.com>
2023-09-05 01:25:45 +0530

    vca6

--- src/utility/apiConfig.js
+++ src/utility/apiConfig.js
@@ -5,13 +5,8 @@
 // export const CURRENT_BASE_URL = 'http://localhost:3000'
 
 // PRODUCTION
-
 export const BASE_URL = 'http://34.31.210.3:8084/api'
 
 export const CURRENT_BASE_URL = 'http://3.110.190.235:3000'
-
-
-
-
 
 
